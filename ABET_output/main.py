e3a55cc1878f97749ca8c1de8be49a264ef7377a
GIGLI029 <gigli029@umn.edu>
2021-11-26 16:55:36 -0600

    adding new base file for repository

---
+++ ABET_output/main.py
@@ -0,0 +1,45 @@
+"""
+Contains the bones of the code I'm pulling from sql_ABET_analyzer; as run, should check "inputs" folder for .ABETdb files, process any databases there, and print files for each run schedule named by the animal ID + date into the output folder. 
+"""
+
+import sqlite3
+import os
+import time
+
+from calc2 import calc2
+from userDefinitions2 import userDefinitions2
+from extractUserData2 import extractUserData2
+from createSQLiteDBs import createSQLiteDBs
+from summary import summaryPres
+from remSess import remSess
+from sortCSVfile import sortCSVfile
+
+input1 = 0 #specifying what 
+extractSet = 1
+extractIDs = 1
+extractDAYS = 1
+extractSch = 1
+extractComp = 0
+
+workingDir = os.getcwd()
+loop = 0
+DBsFound = []
+for i in os.listdir(os.curdir):                             #find file names
+if i.__contains__(".ABETdb"):
+    fileName.append(i)
+for i in fileName:                                          #convert each found file to csv files and place them in folder
+Pcommand = "bash mdb-export-all.sh " + i
+p = subprocess.Popen(Pcommand, stdout = subprocess.PIPE, close_fds = True, shell = True)
+stdout, stderr = p.communicate()
+for i in fileName:                                          #move the files to the main folder, and convert each batch of relevant csv files a single .mdb file
+folderName, fileExt = fileName[loop].split(".")
+folderName = str(folderName) + str('/')
+file1, file2, file3, file4 = (str(folderName) + str('tbl_Data.csv')), (str(folderName) + str('tbl_Schedules.csv')), (str(folderName) + str('tbl_Schedule_Notes.csv')), str(folderName) + str('tbl_Version.csv')
+shutil.copy2(file1, 'tbl_Data.csv')
+shutil.copy2(file2, 'tbl_Schedules.csv')
+shutil.copy2(file3, 'tbl_Schedule_Notes.csv')
+shutil.copy2(file4, 'tbl_Version.csv')
+dbName, dbExt = fileName[loop].split('.')
+folderRM = dbName
+dbName = dbName + str('.db')
+DBsFound.append(dbName)
---
+++ ABET_output/pipeline.md
@@ -0,0 +1,7 @@
+Contains notes, planned pipeline, etc.
+
+pipeline for analysis: 
+
+take db file
+
+extract pieces day by day
