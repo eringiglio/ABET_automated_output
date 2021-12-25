8afe7fefe2ab7868ce17a88d9decdec15b04eae4
GIGLI029 <gigli029@umn.edu>
2021-12-24 22:13:20 -0600

    removing index written, beginning to put pieces together, adding chamber

--- ABET_output/global_csv_processing.py
+++ ABET_output/global_csv_processing.py
@@ -1,34 +1,38 @@
 """
 Intended to pull the output of any given DB folder into a set of folders and files including: 1) a daily summary output for all mice run on a given day; 2) folders per mouse with a csv file for each mouse per day. EMG
+
+#use: db is ABET database, to be read global_database('name of ABETdb') WITHOUT the file end tabs. so if your database file is db_28.ABETdb, the use of this routine should be something like dailyList = global_database(db_28). Optional return of dailyList will just create a list of the files that you created this round; you don't need to keep or read it, everything should be written to csv.
 """
 import os
 import pandas as pd
 import datetime as dt
+import subprocess
 
-dbFolder = os.path.abspath(os.curdir + '/outputs/female9-12_DB27/') #get the folder path for where I am right now
+def global_datapull(db): 
+    dbFolder = os.path.abspath(os.curdir + '/outputs/' + db + '/') #get the folder path for where I am right now
+    dataDF = pd.read_csv(dbFolder+'/tbl_Data.csv') #note that this syntax, whether or not there's the slash, seems to be an annoying powershell thing that may need tweaking
+    scheduleDF = pd.read_csv(dbFolder+'/tbl_Schedules.csv',index_col='SID')
+    notesDF = pd.read_csv(dbFolder+'/tbl_Schedule_Notes.csv')
+    #okay. so these things are all given specific SIDs which have animal ids, who runs them, etc. We want to first construct a list of all of these IDs, which can be used to pull the correct data from all individuals...
+    #here we will want to specify
+    scheduleDF['date'] = [dt.datetime.strptime(i, '%m/%d/%y %H:%M:%S').date() for i in scheduleDF.SRunDate]
+    scheduleDF['time'] = [dt.datetime.strptime(i, '%m/%d/%y %H:%M:%S').time() for i in scheduleDF.SRunDate]
 
-dataDF = pd.read_csv(dbFolder+'/tbl_Data.csv') #note that this syntax, whether or not there's the slash, seems to be an annoying powershell thing that may need tweaking 
-scheduleDF = pd.read_csv(dbFolder+'/tbl_Schedules.csv',index_col='SID')
-notesDF = pd.read_csv(dbFolder+'/tbl_Schedule_Notes.csv')
+    #finding all animals in the dataframe...
+    SIDlist = dataDF.SID.unique()
+    dailyList = []
 
-#okay. so these things are all given specific SIDs which have animal ids, who runs them, etc. We want to first construct a list of all of these IDs, which can be used to pull the correct data from all individuals...
-#here we will want to specify
-scheduleDF['date'] = [dt.datetime.strptime(i, '%m/%d/%y %H:%M:%S').date() for i in scheduleDF.SRunDate]
-scheduleDF['time'] = [dt.datetime.strptime(i, '%m/%d/%y %H:%M:%S').time() for i in scheduleDF.SRunDate]
-
-#finding all animals in the dataframe...
-SIDlist = dataDF.SID.unique()
-
-#identifying by date--optionally, shrink this down to the SIDs run on a particular day
-for i in scheduleDF.date.unique():
-    dateFolder = os.path.abspath(dbFolder + '/' + i.strftime('%m-%d-%y') + '/')
-    if os.path.isdir(dateFolder) is False: #makes sure we have a date-specific outputs folder for individual animals' information
-        os.makedirs(dateFolder)
-    scheduleDay = scheduleDF[scheduleDF.date == i]
-    thisSIDlist = scheduleDay.index.unique() #at which point we can define SIDlist as these numbers only. remember, for scheduleDF but NOT dataDF the scheduleID is the index ID.
-    outputSummary = []
-    #outputHeaders = ['mouseID','date_run','scheduleName','numTrialsCompleted','PercentCorrect','numTrialsCorrect','numReversals']
-    outputHeaders = ['mouseID','date_run','start_time','scheduleName','numTrialsCompleted','maxTrialLength','timeTrialCompleted','whoRan'] #v2 will involve figuring some of this out but it would actually be quite useful to have the individual outputs per animal
+    #identifying by date--optionally, shrink this down to the SIDs run on a particular day
+    for i in scheduleDF.date.unique():
+        dateFolder = os.path.abspath(dbFolder + '/' + i.strftime('%m-%d-%y') + '/')
+        if os.path.isdir(dateFolder) is False: #makes sure we have a date-specific outputs folder for individual animals' information
+            os.makedirs(dateFolder)
+        scheduleDay = scheduleDF[scheduleDF.date == i]
+        thisSIDlist = scheduleDay.index.unique() #at which point we can define SIDlist as these numbers only. remember, for scheduleDF but NOT dataDF the scheduleID is the index ID.
+        outputSummary = []
+        #outputHeaders = ['mouseID','date_run','scheduleName','numTrialsCompleted','PercentCorrect','numTrialsCorrect','numReversals','maxTrialLength','timeTrialCompleted','whoRan']
+        outputHeaders = ['mouseID','date_run','start_time','chamber','scheduleName','numTrialsCompleted','maxTrialLength','timeTrialCompleted','whoRan'] #v2 will involve figuring some of this out but it would actually be quite useful to have the individual outputs per animal
+        #still missing: 'PercentCorrect','numTrialsCorrect','numReversals',
     for j in thisSIDlist: #within each day....
         thisSch = scheduleDF.loc[j]
         if thisSch.SName == 'TestLines-PAL':
@@ -37,18 +41,20 @@ for i in scheduleDF.date.unique():
         noteMatrix = notesDF[notesDF.SID==j]
         thisNotes = pd.DataFrame(data=list(noteMatrix.NValue),index=noteMatrix.NName).T
         totalNumTrials = max(thisData[thisData.DEffectText=="_Trial_Counter"].DValue1)
-        #make the individual CSV per animal 
+        #make the individual CSV per animal
         thisData['scheduleID'] = thisSch.SName
         animal_outfile = dateFolder + '/' + thisNotes['Animal ID'][0] + '_'+ i.strftime('%m-%d-%y') + '.csv'
-        thisData.to_csv(animal_outfile)
+        thisData.to_csv(animal_outfile,index=False)
         for key in thisNotes.keys():
             if 'who' in key.lower():
                 whoKey = key #we don't name all the keys the same thing but some variant on 'who ran' is there, so I instruct the program to hunt through the keys for the name 'who'
-        outputSummary.append([thisNotes['Animal ID'][0],i.strftime('%m/%d/%y'),thisSch.time,thisSch.SName,totalNumTrials,thisNotes.Max_Schedule_Time.loc[0],max(thisData.DTime),thisNotes[whoKey].loc[0]])
-    #daily summary csv
-    dailyDF = pd.DataFrame(data=outputSummary,columns=outputHeaders)
-    dailyFile = dateFolder + '/' + i.strftime('%m-%d-%y') + '_summary.csv'
-    dailyDF.to_csv(dailyFile)
+        outputSummary.append([thisNotes['Animal ID'][0],i.strftime('%m/%d/%y'),thisSch.time,thisSch.SEnviro,thisSch.SName,totalNumTrials,thisNotes.Max_Schedule_Time.loc[0],max(thisData.DTime),thisNotes[whoKey].loc[0]])
+        #daily summary csv
+        dailyDF = pd.DataFrame(data=outputSummary,columns=outputHeaders)
+        dailyFile = dateFolder + '/' + i.strftime('%m-%d-%y') + '_summary.csv'
+        dailyDF.to_csv(dailyFile,index=False)
+        dailyList.append(dailyFile) #just a list for recordkeeping if you want to check it
+    return()
 
 #note for self: "percent correct" only really applies to some schedules: 80/20, 90/10, 100/1, but *not* BANDIT necessarily or the very early schedules. get a list: which schedules include this information, and under what nomenclature?
 #schedules with metrics for 'correct': cue no reward 90-10 spatial built in reversal...
--- ABET_output/main.py
+++ ABET_output/main.py
@@ -1,14 +1,14 @@
 """
-Contains the bones of the code I'm pulling from sql_ABET_analyzer; as run, should check "inputs" folder for .ABETdb files, process any databases there, and print files for each run schedule named by the animal ID + date into the output folder.
+Contains the bones of the code I'm pulling from sql_ABET_analyzer; as run, should check "inputs" folder for .ABETdb files, process any databases there, and save files for each run schedule named by the animal ID + date into the output folder. 
 """
 
 #actually used
 import os
 import subprocess
+from ABET_output.global_csv_processing import global_datapull
 
-
-#outputDir = '/outputs/' #outputs should be assigned to the output folder within the directory, but that's done in mdb. 
-inputDir = '/mnt/g/My Drive/Coding/ABET_automated_output/inputs/' #ABETdb files should go into the input folder.
+inputDir = 'G:/My Drive/Coding/ABET_automated_output/inputs/' #ABETdb files should go into the input folder.
+outputDir = 'G:/My Drive/Coding/ABET_automated_output/outputs/' #note that where the output files go to is written in the mdb-export file.  
 
 workingDir = os.getcwd()
 loop = 0
@@ -18,5 +18,12 @@ for i in os.listdir(inputDir):                             #find file names in i
         fileName.append(i)
 
 for i in fileName:                                          #convert each found file to csv files and place them in folder
-    Pcommand = workingDir + "/mdb-export-all.sh", inputDir + i
+    Pcommand = workingDir + "/mdb-export-all.sh", inputDir + i #mdb-export-all defines where these are written 
     subprocess.run(Pcommand)
+
+#this will create a bunch of files in the output folder. next... 
+dbList = os.listdir(outputDir)
+for db in dbList:
+    print(db)
+    global_datapull(db)
+
