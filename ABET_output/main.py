955d8014ab578006b87273c01ab2b687320c24a3
Erin Giglio <eringiglio@gmail.com>
2021-12-30 13:39:46 -0600

    adding a summary folder that contains copies of all summary pages

--- ABET_output/main.py
+++ ABET_output/main.py
@@ -1,42 +1,46 @@
 """
-Contains the bones of the code I'm pulling from sql_ABET_analyzer; as run, should check "inputs" folder for .ABETdb files, process any databases there, and save files for each run schedule named by the animal ID + date into the output folder. 
+Contains the bones of the code I'm pulling from sql_ABET_analyzer; as run, should check "inputs" folder for .ABETdb files, process any databases there, and save files for each run schedule named by the animal ID + date into the output folder.
 """
 
 #actually used
 import os
 import subprocess
 from ABET_output.global_csv_processing import global_datapull
+from ABET_output.merge_chambers import merge_chambers
 
 inputDir = 'G:/My Drive/Coding/ABET_automated_output/inputs/' #ABETdb files should go into the input folder.
-outputDir = 'G:/My Drive/Coding/ABET_automated_output/outputs/' #note that where the output files go to is written in the mdb-export file.  
-finalFolder = "G:/Shared drives/Grissom Lab UMN/ABETdata/CSV/"
-dbBackupDir = "G:/Shared drives/Grissom Lab UMN/ABETdata/ABETdata/unpacked_databases/"
+outputDir = 'G:/My Drive/Coding/ABET_automated_output/outputs/' #note that where the output files go to is written in the mdb-export file.
+finalDir = "G:/Shared drives/Grissom Lab UMN/ABETdata/CSV/" #this is where all of the mouse-day files go, along with a copy of the daily summary for that day
+dbBackupDir = "G:/Shared drives/Grissom Lab UMN/ABETdata/ABETdata/unpacked_databases/" #raw unpacked csv files for each database go here
+summaryDir = "G:/Shared drives/Grissom Lab UMN/ABETdata/ABETdata/daily_summaries/" #another copy of each daily summary file goes here
 
 workingDir = os.getcwd()
-loop = 0
 fileName = []
 for i in os.listdir(inputDir):                             #find file names in input folder
     if i.__contains__(".ABETdb"):
         fileName.append(i)
 
+fileName.sort()
+
 for i in fileName:                                          #convert each found file to csv files and place them in folder
-    Pcommand = workingDir + "/mdb-export-all.sh", inputDir + i #mdb-export-all defines where these are written 
+    Pcommand = workingDir + "/mdb-export-all.sh", inputDir + i #mdb-export-all defines where these are written
     subprocess.run(Pcommand)
 
 #let's back these up here. no reason to waste perfectly good csv explosions. send those...
 dbList = os.listdir(outputDir)
 dbList.sort()
 
-#this will create a bunch of files in the output folder. next, pull those csvs apart into the animal-day csvs with summaries...... 
+#this will create a bunch of files in the output folder. next, pull those csvs apart into the animal-day csvs with summaries......
 for db in dbList: #for every database in our list of databases...
     print(db)
     newDBdir = dbBackupDir + db + '/'
-    if os.path.isdir(dbBackupDir) is False: #if there isn't already a folder for this database in the folder for backing up the database CSV intermediate files, make one 
+    if os.path.isdir(dbBackupDir) is False: #if there isn't already a folder for this database in the folder for backing up the database CSV intermediate files, make one
         os.makedirs(dbBackupDir)
     oldDBdir = outputFolder+db+'/'
     for file in os.listdir(oldDBdir): #"for every csv file in the folder of interest, read it, move it over to the new directory for storing these, and write it over there so we have a record of the intermediate file"
         thisFile = pd.read_csv(oldDBdir+file) #read old file
         newFN = newDBdir + file #make a new path to put a new file into
-        thisFile.to_csv(newFN,index=False) #put this csv at that place 
-    out = global_datapull(db) #from the global_datapull: go ahead and take all those csvs and create a bunch of helpful little syncing tips 
+        thisFile.to_csv(newFN,index=False) #put this csv at that place
+    out = global_datapull(db) #from the global_datapull: go ahead and take all those csvs and create a bunch of helpful little syncing tips
 
+merge_chambers(outputDir,finalDir,summaryDir)
--- ABET_output/merge_chambers.py
+++ ABET_output/merge_chambers.py
@@ -1,19 +1,19 @@
 #I would like to have all these outputs go to single folders per date, not separated by chamber. So...
 
 import os
-import pandas as pd 
+import pandas as pd
 import subprocess
 
-def merge_chambers(outputFolder,finalFolder):
+def merge_chambers(outputFolder,finalFolder,summaryFolder):
     #goal is to consolidate all four items into one thing and move to a final placement...
     dbList = os.listdir(outputFolder)
     dateList = []
-    dbDict = {} 
+    dbDict = {}
     for db in dbList:
         dirList = [a for a in os.listdir(outputFolder+db) if os.path.isdir(os.path.join(outputFolder,db,a)) is True]
         dbDict[db] = {'dates' : dirList}
         dateList = dateList + dirList
-    #total list of dates for the list 
+    #total list of dates for the list
     dateList=list(set(dateList))
     dateList.sort()
     for date in dateList:
@@ -28,12 +28,14 @@ def merge_chambers(outputFolder,finalFolder):
                     thisFile = pd.read_csv(oldDateFolder+'/'+file)
                     newFN = newDateFolder + '/' + file
                     if 'summary' in file:
-                        #check the old one 
+                        #check the old one
                         try:
                             oldSummary = pd.read_csv(newFN)
                             newDF = pd.concat([oldSummary,thisFile])
                             newDF.to_csv(newFN,index=False)
+                            newSF = summaryFolder+file
+                            newDF.to_csv(newSF,index=False)
                         except:
-                            thisFile.to_csv(newFN,index=False)                        
+                            thisFile.to_csv(newFN,index=False)
                     else:
                         thisFile.to_csv(newFN,index=False)
