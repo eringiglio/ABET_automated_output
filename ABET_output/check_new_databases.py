79e85a47520f84e1de465386838531230f29da65
Erin <gigli029@umn.edu>
2022-01-05 10:45:09 -0600

    solving problem with timestamps, fuck rclone os is my best friend now

--- ABET_output/check_new_databases.py
+++ ABET_output/check_new_databases.py
@@ -1,25 +1,34 @@
 """
 Should check the computer files for any recent database updates, copy the new database files over to database directories in the ABETdb Google Drive, and unpack ONLY databases which have recently been updated.
 
-Because this file is looking at folders which are backed up to Google Drive, rather than at the shared drive we host the files on itself, we need to use rclone to check on and copy these files instead of standard Python file reading, copying, and moving protocols.
 """
 #import modules
 import pandas as pd
-import subprocess
+import os
 import numpy as np
+from datetime import datetime, timedelta
 
 rclone = "H:/Shared drives/Grissom Lab UMN/ABETdata/rclone-v1.57.0/rclone.exe" #should be the full path to rclone on your installation
 
-#this should be a dictionary of folders which you want to copy from a watched PC's ABET output directory to a secondary directory (to reduce any risk of file corruption while a database may be in use). Keys should be the original ABET System Folder location; values should be the location of this secondary directory. Note that copydict frames the shared drive in terms of the name given to that drive in rclone's config; rather than G:/Shared drives/Grissom Lab UMN, we see instead "UMNdrive"
+#this should be a dictionary of folders which you want to copy from a watched PC's ABET output directory to a secondary directory (to reduce any risk of file corruption while a database may be in use). Keys should be the original ABET System Folder location; values should be the location of this secondary directory.
 copyDict = {
-    "UMNdrive:/ABETdata/ABETdb/chamber1-4/" : "UMNdrive:/ABETdata/ABETdb/ch1-4/",
-    "UMNdrive:/ABETdata/ABETdb/chamber5-8/" : "UMNdrive:/ABETdata/ABETdb/ch5-8/",
-    "UMNdrive:/ABETdata/ABETdb/chamber9-12/" : "UMNdrive:/ABETdata/ABETdb/ch9-12/",
-    "UMNdrive:/ABETdata/ABETdb/chamber13-16/" : "UMNdrive:/ABETdata/ABETdb/ch13-16/"}
+    "H:/Other computers/chamber 1-4/ABET System Folder/" : "H:/Shared drives/Grissom Lab UMN/ABETdata/ABETdb/ch1-4/",
+    "H:/Other computers/chamber 5-8/ABET System Folder/" : "H:/Shared drives/Grissom Lab UMN/ABETdata/ABETdb/ch5-8/",
+    "H:/Other computers/chamber 9-12/ABET System Folder/" : "H:/Shared drives/Grissom Lab UMN/ABETdata/ABETdb/ch9-12/",
+    "H:/Other computers/chamber 13-16/ABET System Folder/" : "H:/Shared drives/Grissom Lab UMN/ABETdata/ABETdb/ch13-16/"}
 
 for i in copyDict:
-    Pcommand = rclone + " ls " + copyDict[i]
-    out = subprocess.run(Pcommand,capture_output=True)
+    fileList = os.listdir(i)
+    for j in fileList:
+        if '.ABETdb' in j:
+            fullPath = i+j
+            stat_result = os.stat(fullPath)
+            lastModified = datetime.fromtimestamp(stat_result.st_mtime)
+            if lastModified + timedelta(days=5) > datetime.now():
+
+
 
 #this results in a binary ist, but we want a list of strings...
-no.
+np.unpackbits(out.stdout)
+
+out
