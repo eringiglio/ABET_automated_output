3f2df79c361e4a000c16e883176d71d47c2e1ab5
Erin <gigli029@umn.edu>
2022-01-05 13:23:27 -0600

    added a routine to clear out the input folder before re-filling it

--- ABET_output/check_new_databases.py
+++ ABET_output/check_new_databases.py
@@ -11,20 +11,28 @@ import shutil
 
 #this should be a dictionary of folders which you want to copy from a watched PC's ABET output directory to a secondary directory (to reduce any risk of file corruption while a database may be in use). Keys should be the original ABET System Folder location; values should be the location of this secondary directory.
 
-copyDict = {
-    "H:/Other computers/chamber 1-4/ABET System Folder/" : "H:/Shared drives/Grissom Lab UMN/ABETdata/ABETdb/ch1-4/",
-    "H:/Other computers/chamber 5-8/ABET System Folder/" : "H:/Shared drives/Grissom Lab UMN/ABETdata/ABETdb/ch5-8/",
-    "H:/Other computers/chamber 9-12/ABET System Folder/" : "H:/Shared drives/Grissom Lab UMN/ABETdata/ABETdb/ch9-12/",
-    "H:/Other computers/chamber 13-16/ABET System Folder/" : "H:/Shared drives/Grissom Lab UMN/ABETdata/ABETdb/ch13-16/"}
-
-for i in copyDict:
-    fileList = os.listdir(i)
-    for j in fileList:
-        if '.ABETdb' in j:
-            fullPath = i+j
-            stat_result = os.stat(fullPath)
-            lastModified = datetime.fromtimestamp(stat_result.st_mtime)
-            if lastModified + timedelta(days=1) > datetime.now(): #if you have last modified the ABETdb file within 1 days...
-                print(j)
-                newPath = copyDict[i] + j
-                shutil.copy(fullPath,newPath) #...copy that file to the other directory where they live.
+def check_new_databases():
+    db_inPath = "H:/Shared drives/Grissom Lab UMN/ABETdata/ABETdb/db_inputs/"
+    for db in os.listdir(db_inPath): #just remove everything in this folder to make things nice and tidy 
+        os.remove(os.join(db_inPath,db))
+    copyDict = {
+        "H:/Other computers/chamber 1-4/ABET System Folder/" : "H:/Shared drives/Grissom Lab UMN/ABETdata/ABETdb/ch1-4/",
+        "H:/Other computers/chamber 5-8/ABET System Folder/" : "H:/Shared drives/Grissom Lab UMN/ABETdata/ABETdb/ch5-8/",
+        "H:/Other computers/chamber 9-12/ABET System Folder/" : "H:/Shared drives/Grissom Lab UMN/ABETdata/ABETdb/ch9-12/",
+        "H:/Other computers/chamber 13-16/ABET System Folder/" : "H:/Shared drives/Grissom Lab UMN/ABETdata/ABETdb/ch13-16/"}
+    dbList = []
+    for i in copyDict:
+        fileList = os.listdir(i)
+        for j in fileList:
+            if '.ABETdb' in j:
+                fullPath = i+j
+                stat_result = os.stat(fullPath)
+                lastModified = datetime.fromtimestamp(stat_result.st_mtime)
+                if lastModified + timedelta(days=1) > datetime.now(): #if you have last modified the ABETdb file within 1 days...
+                    print(j)
+                    newPath = copyDict[i] + j
+                    dbPath = db_inPath + j
+                    dbList.append(j)
+                    shutil.copy(fullPath,newPath) #...copy that file to the other directory where they live.
+                    shutil.copy(fullPath,dbPath) #and also, ideally, to a single input folder I can just pull from because c'mon.
+    return(dbList)
